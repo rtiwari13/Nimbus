98e85391db65980d8824a91d70d9faefa186f815
rtiwari13 <ritanshutiwari593@gmail.com>
2025-04-25 09:42:28 +0530

    fixed setup

---
+++ nimbus_frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return  (
+        <div className="bg-red-600">
+           <h1>HI this is setup for NIMBUS</h1>
+     
+        </div>
+    )
+}
+
+export default HomePage;
